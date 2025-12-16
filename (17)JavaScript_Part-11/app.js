f1b2d810f84632a235f05cf3668ff3d4ea1f7a41
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-12-16 22:14:33 +0530

    app.js modified

--- (17)JavaScript_Part-11/app.js
+++ (17)JavaScript_Part-11/app.js
@@ -1,65 +1,95 @@
-//callback hell example
+// //callback hell example
 
-function isSuccess(data, success, failure) {
-    let internetSpeed = Math.floor(Math.random() * 10 + 1);
+// function isSuccess(data, success, failure) {
+//     let internetSpeed = Math.floor(Math.random() * 10 + 1);
 
-    if (internetSpeed > 4) {
-        console.log(`Data : ${data}`);
-        success();
-    } else {
+//     if (internetSpeed > 4) {
+//         console.log(`Data : ${data}`);
+//         success();
+//     } else {
 
-        console.log(`Data : ${data}`);
-        failure();
-    }
-}
+//         console.log(`Data : ${data}`);
+//         failure();
+//     }
+// }
 
-isSuccess(
-    "Data1",
+// isSuccess(
+//     "Data1",
 
-    () => {
-        console.log("Data inserted successfully");
+//     () => {
+//         console.log("Data inserted successfully");
 
-        //data1 inserted, so now insert data2
-        isSuccess(
-            "Data2",
+//         //data1 inserted, so now insert data2
+//         isSuccess(
+//             "Data2",
 
-            () => {
-                console.log("Data inserted successfully");;
-            },
+//             () => {
+//                 console.log("Data inserted successfully");;
+//             },
 
-            () => {
-                console.log("Data insertion failed")
-            }
-        );
-    },
+//             () => {
+//                 console.log("Data insertion failed")
+//             }
+//         );
+//     },
 
-    () => {
-        console.log("Data insertion failed")
-    }
+//     () => {
+//         console.log("Data insertion failed")
+//     }
 
-);
+// );
 
 
-//Promise
-function insertToDb(data) {
+// //Promise
+// function insertToDb(data) {
+//     return new Promise((resolve, reject) => {
+//         let internetSpeed = Math.floor(Math.random * 10) + 1;
+
+//         if (internetSpeed > 4) {
+//             resolve("Success : data was saved");
+//         } else {
+//             reject("Failure: weak connection");
+//         }
+
+//     }
+//     );
+// }
+
+// let request = insertToDb("Hello");
+// request
+//     .then(() => {
+//         console.log("Promise resolved");
+//     })
+//     .catch(() => {
+//         console.log("Promise rejected");
+//     });
+
+
+function changeColor(color, delay) {
     return new Promise((resolve, reject) => {
-        let internetSpeed = Math.floor(Math.random * 10) + 1;
+        setTimeout(() => {
+            const h1 = document.querySelector("h1");
 
-        if (internetSpeed > 4) {
-            resolve("Success : data was saved");
-        } else {
-            reject("Failure: weak connection");
-        }
-
-    }
-    );
+            if (h1) {
+                h1.style.color = color;
+                resolve("Color changed to " + color);
+            } else {
+                reject("h1 element not found");
+            }
+        }, delay);
+    });
 }
 
-let request = insertToDb("Hello");
-request
-    .then(() => {
-        console.log("Promise resolved");
+
+changeColor("red", 2000)
+    .then((result) => {
+        console.log(result);
+
+        return changeColor("blue", 2000);
     })
-    .catch(() => {
-        console.log("Promise rejected");
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((error) => {
+        console.log(error);
     });
